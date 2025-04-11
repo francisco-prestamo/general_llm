120d5f9148836a8177d09d3b76272e35cd833fd8
Juan Carlos <juancarlosmatcom@gmail.com>
2025-04-11 12:07:14 -0400

    Vectorizando Respuesta

--- evaluator.py
+++ evaluator.py
@@ -2,6 +2,10 @@ from fireworks.client import Fireworks
 import os
 import numpy as np
 from sklearn.preprocessing import normalize
+from dotenv import load_dotenv  # Importar dotenv
+import openai
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
 
 def Response(model, prompt):
     api_key = os.getenv("API_KEY")
@@ -19,31 +23,24 @@ def Response(model, prompt):
 
     return response.choices[0].message.content
 
-def Vectorize(model, promt):
+def vectorizar_texto(model, promt) -> np.ndarray:
     api_key = os.getenv("API_KEY")
     if not api_key:
         raise ValueError("API_KEY no encontrado en el archivo .env")
     
     client = Fireworks(api_key=api_key)
     response = client.embeddings.create(
-        model="test-embeding-ada-002",
-        input=promt,
-    )
-    return response.data[0].embedding
-
-def vectorizar_texto(texto: str, modelo: str = "fireworks-ai/text-embedding") -> np.ndarray:
-    response = Fireworks.client.Embedding.create(
-        model=modelo,
-        input=[texto]
+        model=model,
+        input=[promt]
     )
     vector = np.array(response.data[0].embedding)
     return normalize([vector])[0]  # Normalización L2
 
 def main():
     # Configuración
-    model_response = "gpt-3.5-turbo"  # Modelo para generar respuestas
-    model_vectorize = "text-embedding-ada-002"  # Modelo para vectorizar
-    prompt = ""
+    model_response = "accounts/fireworks/models/llama-v3p3-70b-instruct"  # Modelo para generar respuestas
+    model_vectorize = "nomic-ai/nomic-embed-text-v1.5"  # Modelo para vectorizar
+    prompt = "Cual es el color del caballo blanco de maceo"
 
     try:
         # Obtener respuesta del modelo
@@ -53,7 +50,7 @@ def main():
 
         # Vectorizar la respuesta
         print("Vectorizando la respuesta...")
-        vector = Vectorize(model=model_vectorize, promt=response_text)
+        vector = vectorizar_texto(model=model_vectorize, promt=response_text)
         print(f"Vector obtenido: {vector}")
 
     except ValueError as e:
