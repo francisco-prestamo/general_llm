128a6ebec147b41b1799c8e1b2279f6da77e7cf8
Darian <133162157+Srdariansitop@users.noreply.github.com>
2025-04-11 10:54:15 -0400

    Update evaluator.py

--- evaluator.py
+++ evaluator.py
@@ -1,12 +1,14 @@
 from fireworks.client import Fireworks
 
-client = Fireworks(api_key="<FIREWORKS_API_KEY>")
-response = client.chat.completions.create(
-model="accounts/fireworks/models/llama-v3p3-70b-instruct",
-messages=[{
-   "role": "user",
-   "content": "Say this is a test",
-}],
-)
+def Response(model, prompt):
+    client = Fireworks(api_key="fw_3ZPqtKzG9NqJXzWPuxpjiYKo")
+    response = client.chat.completions.create(
+    model=model,
+    messages=[{
+        "role": "user",
+        "content": prompt,
+    }],
+    )
 
-print(response.choices[0].message.content)
+    return response.choices[0].message.content
+
